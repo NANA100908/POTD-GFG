ec8aa0c490d91221538ebeb1910e0d69d1da252b
Prerna Kalwani <kalwaniprerna16@gmail.com>
2024-05-19 23:47:03 +0530

    Create Find_The_Highest_Number.cpp

---
+++ Find_The_Highest_Number.cpp
@@ -0,0 +1,15 @@
+// Solution:
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        int i=0;
+        while(a[i+1]> a[i] && i<a.size()-1){
+            i++;
+        }
+        return a[i];
+    }
+};
+
